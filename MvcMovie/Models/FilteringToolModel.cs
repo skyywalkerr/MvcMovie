ce6545234057acff4b6f49f1ef7816b3b01bfabf
Luke <lcichocki@wsflab.com>
2015-04-14 12:22:54 -0500

    complete upload

---
+++ MvcMovie/Models/FilteringToolModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMovie.Models
+{
+    public class FilteringToolModel
+    {
+            public string Department { get; set; }
+            public string WorkCenter { get; set; }
+            public string CheckBox { get; set; }
+
+    }
+}
